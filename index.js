2e564802d31732af74f580f588eb40421fbef7e2
ShanchaiZ <shanchai.zahid@gmail.com>
2022-11-22 23:52:26 -0500

    required the dependencies in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//Installed and require the dependencies:
+const franc = require("franc");
+const langs = require("langs");
+const colors = require("colors");
+
+
+
+console.log(franc('Alle menslike wesens word vry'));
