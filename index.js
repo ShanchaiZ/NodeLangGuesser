635908a02284dcff1fc6ecccb0d29943b7613c00
ShanchaiZ <shanchai.zahid@gmail.com>
2022-11-23 16:54:08 -0500

    enabled and added color npm and color at end of strings for aesthetics

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const require = createRequire(import.meta.url);
 //Installed and require the dependencies:
 import { franc } from "franc";
 const langs = require("langs");
-// const colors = require("colors");
+const colors = require("colors");
+
+//Language detection code:
 const input = process.argv[2]; //node user input 
 const langCode = franc(input); //franc = method to detect the language of text
 
 if (langCode === "und") {
-    console.log("Undetected Language. Please try again with more sample text!")
+    console.log("Undetected Language. Please try again with more sample text!".bgMagenta);
 } else {
     const langugage = langs.where("3", langCode); //lang = language library that represents languages in 3 language codes.
-    console.log(`Our best guess is: ${langugage.name}`);
+    console.log(`Our best guess is: ${langugage.name}`.brightBlue);
 }
 
 
@@ -37,3 +39,4 @@ if (langCode === "und") {
 // 2. Declared variables where franc is used to Detect the language of text and langs is a library that has all the language coded in ISO 639-1/2/3 language codes.
 //3. Use user input for language to be detected by code. In node process.argv[0] is the executable path, process.argv[1] is the path to file and process.argv[2] is other arguments. we want to use process.argv[2] as user inputs for language.
 //4. If language detection fails => console.log "try again with more sample text!"
+//5. BONUS: added npm colors for aesthetics. Installed and add at the end of strings to make color take effect!
