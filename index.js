6e06fb979c35f686b687b5e902d5c8536951197f
ShanchaiZ <shanchai.zahid@gmail.com>
2022-11-23 15:44:25 -0500

    used 'import' syntax instead of 'required' for 'franc' version 6 and added <type:module> in package.json

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //Installed and require the dependencies:
-const franc = require("franc");
+import {franc} from "franc";
 const langs = require("langs");
 const colors = require("colors");
 
 
 
 console.log(franc('Alle menslike wesens word vry'));
+
+
+
+
+//Challenges:
+//problem 1: franc version 6 is written in ES module and not commonJS. This means "required" doesnt work.
+//solution 1: Use import from their site AND input <"type": "module"> in the package.json OR RENAME index.js to index.mjs
